1401d052212a51a0c0b6b05f5832f3d97efe90b3
tamir <tamirkhy@gmail.com>
2025-05-13 20:27:32 +0300

    Adjust eslint.config.js imports and formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,38 @@
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import prettier from "eslint-config-prettier";
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintPlugin from '@typescript-eslint/eslint-plugin'
+import eslintParser from '@typescript-eslint/parser'
+import prettier from 'eslint-config-prettier'
 
-export default tseslint.config(
-    {ignores: ["dist", "node_modules", ".vite"]},
-    {
-        files: ["**/*.{ts,tsx}"],
-        languageOptions: {
-            ecmaVersion: "latest",
-            globals: {...globals.browser, ...globals.node},
-            parser: tseslint.parser,
-            sourceType: "module",
-            project: "./tsconfig.json",
-        },
-        plugins: {
-            "@typescript-eslint": tseslint.plugin,
-            "react-hooks": reactHooks,
-            "react-refresh": reactRefresh,
-        },
-        extends: [
-            tseslint.configs.recommended,
-            reactHooks.configs.recommended,
-            prettier,
-        ],
-        rules: {
-            "react-refresh/only-export-components": [
-                "warn",
-                {allowConstantExport: true},
-            ],
-            "@typescript-eslint/no-unused-vars": "warn",
-            "@typescript-eslint/no-explicit-any": "warn",
-            "@typescript-eslint/prefer-optional-chain": "warn",
-            "@typescript-eslint/explicit-function-return-type": "off",
-            "@typescript-eslint/explicit-module-boundary-types": "off",
-            "react/prop-types": "off",
-        },
-        settings: {
-            react: {
-                version: "detect",
-            },
-        },
-    }
-);
+export default {
+  ignores: ['dist', 'node_modules', '.vite'],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    globals: { ...globals.browser, ...globals.node },
+    parser: eslintParser,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: {
+    '@typescript-eslint': eslintPlugin,
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  extends: [eslintPlugin.configs.recommended, reactHooks.configs.recommended, prettier],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
